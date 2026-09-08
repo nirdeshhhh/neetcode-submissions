76716f5caf041a481bd0d65a9dced34ce9a57397
Nirdesh Kumar <nirdeshhhh04@gmail.com>
2026-09-08 08:33:33 +0530

    Add: is-palindrome - submission-0

---
+++ Data Structures & Algorithms/is-palindrome/submission-0.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    bool isPalindrome(string s) {
+        if(s.size() == 0) return true;
+        string cleaned = "";
+
+        for(char c: s){
+            if(isalnum(c)){
+                cleaned += tolower(c);
+            }
+        }
+
+        int left = 0;
+        int right = cleaned.size() -1;
+
+        while(left < right){
+            if(cleaned[right] != cleaned[left]){
+                return false;
+            }
+            left++;
+            right--;
+        }
+        return true;
+    }
+};
